02b2934953a67ed854147f64f62c45aa1994d932
Meharab-27 <meharabrahman100115@gmail.com>
2025-09-22 22:12:35 +0600

    added more

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-
-
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/countries'
 
-function App() {
- 
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
+function App() {
   return (
     <>
-      
-     
-     <Countries></Countries>
-     
-     
+      <Suspense fallback={<p>meharin is loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
 
-export default App
+export default App;
+
+
+
+
